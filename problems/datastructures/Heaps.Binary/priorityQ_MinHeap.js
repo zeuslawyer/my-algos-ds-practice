18a93160da9aefb03af48294315ceeed6e7fe44d
zeuslawyer <zubin.pratap@gmail.com>
2019-11-03 10:55:01 +1100

    refactor priority queue to make it more clear with parent child in sinkdown

--- problems/datastructures/Heaps.Binary/priorityQ_MinHeap.js
+++ problems/datastructures/Heaps.Binary/priorityQ_MinHeap.js
@@ -1,7 +1,5 @@
-// NOTE:  uses Nodes to hold data on priority level.
-
-// needs a Node call
-class Node {
+// needs a _Node call
+class _Node {
   constructor(val = null, priority) {
     this.val = val
     this.priority = priority
@@ -16,21 +14,21 @@ class MinBinaryHeap {
 
   // insert
   enque(val, priority) {
-    let insertedNode = new Node(val, priority)
+    let inserted_node = new _Node(val, priority)
 
-    this.nodes.push(insertedNode)
+    this.nodes.push(inserted_node)
     // edge case - heap was empty
     if (this.nodes.length === 1) return this
 
     // else, min heap so  while priority less than parent's priorty bubble up / swap with parent
-    let insertedInd = this.nodes.length - 1  // insertedInd of insertedNode
+    let insertedInd = this.nodes.length - 1  // insertedInd of inserted_node
     let parentInd = Math.floor((insertedInd - 1) / 2)
 
     let count = 0
 
     // min heap so bubble up lesser value as long as index > 0
-    while (insertedInd > 0 && insertedNode.priority < this.nodes[parentInd].priority) {
-      this.swapNodes(insertedInd, parentInd)
+    while (insertedInd > 0 && inserted_node.priority < this.nodes[parentInd].priority) {
+      this.swap_Nodes(insertedInd, parentInd)
 
       // update inserted and its new parent
       insertedInd = parentInd;
@@ -48,7 +46,7 @@ class MinBinaryHeap {
   deque() {
 
     // swap root with last, to make last node the new root
-    this.swapNodes(0, this.nodes.length - 1)
+    this.swap_Nodes(0, this.nodes.length - 1)
 
     // extrac the min value
     let result = this.nodes.pop()
@@ -61,38 +59,42 @@ class MinBinaryHeap {
 
   sinkdown(index = 0) {
     while (index < this.nodes.length) {
+      // set parent
+      let parent = this.nodes[index]
+
       //get the two child indexes
       let leftChildInd = 2 * index + 1
       let rightChildInd = 2 * index + 2
-      let swapIndex;
+      let childInd;
 
       // if both children exist, then swap with the LESSER of them, if needed
       if (this.nodes[leftChildInd] && this.nodes[rightChildInd]) {
-        swapIndex = this.nodes[leftChildInd].priority < this.nodes[rightChildInd].priority ? leftChildInd : rightChildInd
+        // get lesser child
+        childInd = this.nodes[leftChildInd].priority < this.nodes[rightChildInd].priority ? leftChildInd : rightChildInd
 
-        // min heap, swap only if the node is > target swap nodes priority
-        if (this.nodes[index].priority > this.nodes[swapIndex].priority) {
-          this.swapNodes(index, swapIndex)
+        // min heap, swap only if the parent is >child node's priority
+        if (parent.priority > this.nodes[childInd].priority) {
+          this.swap_Nodes(index, childInd)
         }
 
       } else if (!this.nodes[leftChildInd]) {
-        // no more swapping needed, as there are no children here
+        // no more swapping needed, as there are no children here, since in binary heaps left child must exist for a right child to exist
         break
       } else if (!this.nodes[rightChildInd]) {
         // no right child , hence only leftChild so swap with leftChild
-        swapIndex = leftChildInd
+        childInd = leftChildInd
 
-        if (this.nodes[index].priority > this.nodes[swapIndex].priority) {
-          this.swapNodes(index, swapIndex)
+        if (parent.priority > this.nodes[childInd].priority) {
+          this.swap_Nodes(index, childInd)
         }
       }
       // update index for loop evaluation
-      index = swapIndex
+      index = childInd
     }
   }
 
 
-  swapNodes(ind1, ind2) {
+  swap_Nodes(ind1, ind2) {
     // console.log('swapping')
     let temp = this.nodes[ind1]
     this.nodes[ind1] = this.nodes[ind2];
@@ -136,9 +138,9 @@ q.enque('conjunctivitis', 3)
 q.deque()
 // q.deque()
 // q.deque()
-// console.log(q)
-
+console.log(q)
 
 console.log(q.isMinBinHeap())
+
 
 
