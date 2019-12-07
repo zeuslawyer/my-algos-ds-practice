73b195dfd1a354838a5fbbfcfb8f5b4c9a989c50
zeuslawyer <zubin.pratap@gmail.com>
2019-12-07 21:14:19 +1100

    two versions of threeSum

--- problems/algoexpert/threeSum.js
+++ problems/algoexpert/threeSum.js
@@ -1,17 +1,15 @@
 /**
- * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
  * Find all unique triplets in the array which gives the sum of zero
  * Note:
- * The solution set must not contain duplicate triplets.
+ * THE SOLUTION MAY CONTAIN DUPLICATE TRIPLETS - IS OK!, unlike  https://leetcode.com/problems/3sum/ @ threeSumNoDuplicates.js
  * Given array nums = [-1, 0, 1, 2, -1, -4],
  * A solution set is:
- * [ [-1, 0, 1], [-1, -1, 2] ]
- *  https://leetcode.com/problems/3sum/
+ * [ [-1, 0, 1], [-1, -1, 2], [-1, 0, 1] ]
+ *
  */
 
-
-
-function threeNumberSum(arr, target) {
+function threeNumberSum(arr, target = 0) {
   let res = [];
   arr = arr.sort((a, b) => a - b); // sort ascending
 
@@ -24,7 +22,6 @@ function threeNumberSum(arr, target) {
       let second = arr[left];
       let third = arr[right];
       let total = first + second + third;
-      console.log(first, second, third);
 
       // case 1
       if (total === target) {
@@ -44,3 +41,7 @@ function threeNumberSum(arr, target) {
 
   return res;
 }
+
+let u = threeNumberSum([-1, 0, 1, 2, -1, -4], 0);
+
+console.log(u);
