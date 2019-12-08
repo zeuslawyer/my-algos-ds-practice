09203150918cfaffce60c0d27a16c523a13b5b2d
zeuslawyer <zubin.pratap@gmail.com>
2019-12-08 15:09:59 +1100

    move elems to end of array

--- problems/move numbers to end.js
+++ problems/move numbers to end.js
@@ -1,3 +1,15 @@
+// https://leetcode.com/problems/move-zeroes/
+// Given an array nums, write a function to move all 0's
+// to the end of it while maintaining the relative order of the non-zero elements
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+/**
+ * @param {number[]} nums
+ * @param {number} target number to move to end
+ *
+ * @return {number[]}
+ */
 function moveElementToEnd(arr, target) {
   let start = 0;
   let end = arr.length - 1;
@@ -18,10 +30,6 @@ function moveElementToEnd(arr, target) {
   return arr;
 }
 
-let i = moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2); // [1,3,4,2,2,2,2,2]
-
-console.log(i);
-
 function moveElementToEnd2(arr, target) {
   let end = arr.length - 1;
   for (let i = 0; i < end; i++) {
@@ -36,3 +44,10 @@ function moveElementToEnd2(arr, target) {
 
   return arr;
 }
+
+let t = moveElementToEnd([0, 1, 0, 3, 12], 0);
+console.log(t);
+
+let i = moveElementToEnd2([2, 1, 2, 2, 2, 3, 4, 2], 2); // [1,3,4,2,2,2,2,2]
+
+console.log(i);
---
+++ problems/radixSort.js
@@ -0,0 +1,53 @@
+function getDigitAtPos(num, pos, base =10) {
+  num = Math.abs(num) // make positive
+
+  return Math.floor(num/Math.pow(base, pos)) % base
+}
+
+getDigitAtPos(123, 3)
+
+function numOfDigits(num){
+  if (num===0) return 1
+
+  return Math.floor(Math.log10(Math.abs(num)) + 1)
+}
+// numOfDigits(100)
+
+function getMaxDigits(arr){
+  let max = 0
+  for(let i=0; i< arr.length; i++){
+    let digitCount = numOfDigits(arr[i])
+    if (digitCount > max) {
+      max =  digitCount
+    }
+  }
+  return max
+}
+
+getMaxDigits([324,5300000,235245])
+
+function radix(nums){
+  let maxDigits = getMaxDigits(nums)
+
+// outer loop = max digits, c being the position being sorted by
+  for(let c=0; c< maxDigits; c++){
+    // create 10 buckets where index will match digits 0-9
+    let buckets = Array.from({length:10}, ()=>[])
+// inner loop iterate over nums array
+    for(let i=0; i< nums.length; i++) {
+      // sort into buckets by digit pos number
+      let digit = getDigitAtPos(nums[i], c, 10);
+      // digit will be 0-9
+      buckets[digit].push(nums[i])
+    } 
+    // create new nums array from the buckets for next iteration of outer loop
+    nums = [].concat(...buckets)
+  }
+
+  return nums
+}
+
+
+let r = radix([31, 200, 1, 45, 9200])
+
+console.log(r)
