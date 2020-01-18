72583debc6e856098052fa8cc07fd3e16b82d5ed
zeuslawyer <zubin.pratap@gmail.com>
2020-01-18 18:07:33 +1100

    array of array prod in python

--- problems/py_scratch.py
+++ problems/py_scratch.py
@@ -1,3 +1,65 @@
-t = ['oo']
+def arrProd(arr):
+    if len(arr) <= 1:
+        return []
 
-t.insert(0,100)
+    res = [0 for x in range(len(arr))]
+
+    left = [0 for x in range(len(arr))]
+    right = [0 for x in range(len(arr))]
+
+    for i in range(len(arr)):
+        if i == 0:
+            left[i] = 1
+        else:
+            left[i] = (arr[i-1]*left[i-1])
+
+    for i in range(len(arr)-1, -1, -1):
+        if i == len(arr)-1:
+            right[i] = 1
+        else:
+            right[i] = arr[i+1]*right[i+1]
+
+    for i in range(len(arr)):
+        res[i] = left[i] * right[i]
+
+    return res
+
+
+# ans = arrProd([8, 10, 2])
+ans = arrProd([5, 4, 3])
+
+print(ans)
+
+
+def array_of_array_products(arr):
+    size = len(arr)
+    res = [0 for x in range(size)]
+    if size <= 1:
+        return []
+
+    left = [0 for x in range(size)]
+    right = [0 for x in range(size)]
+
+    # left table
+    for i in range(size):
+        if i == 0:
+            left[i] = 1
+        else:
+            left[i] = arr[i-1]*left[i-1]
+
+    # right table
+    for i in range(size-1, -1, -1):
+        if i == size-1:
+            right[i] = 1
+        else:
+            right[i] = arr[i+1]*right[i+1]
+
+    for i in range(size):
+        print(i, size, left, right)
+        res[i] = left[i] * right[i]
+
+    return res
+
+
+ans = array_of_array_products([1])
+print("answer", ans)
--- problems/scratchpad.js
+++ problems/scratchpad.js
@@ -1,6 +1,32 @@
-let str1 = 3;
-let str2 = 5;
+const input = [8, 10, 2];
 
-let t = new Array(str1).fill(new Array(str2));
+function arrOfProd(arr) {
+  const res = [],
+    left = [],
+    right = [];
 
-console.log(t);
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      left[i] = 1;
+    } else {
+      left[i] = arr[i - 1] * left[i - 1];
+    }
+  }
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (i === arr.length - 1) {
+      right[i] = 1;
+    } else {
+      right[i] = arr[i + 1] * right[i + 1];
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    res[i] = left[i] * right[i];
+  }
+
+  return res;
+}
+
+let ans = arrOfProd(input);
+console.log(ans);
