bb93750cfc08c5230fd545cc1010fa9ea3bc1142
zeuslawyer <zubin.pratap@gmail.com>
2020-01-21 22:14:03 +1100

    BST validation

---
+++ problems/algoexpert/BST validation.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ * 
+ * https://leetcode.com/problems/validate-binary-search-tree/submissions/
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    
+  return validateTree(root)
+
+};
+  
+
+function validateTree(node, min = -Infinity, max = Infinity){
+  if (!node) return true
+  
+  if(min >= node.val || max <= node.val) return false
+
+  
+  let leftTree =  validateTree(node.left, min, node.val)
+  let rightTree =validateTree(node.right, node.val, max)
+  
+  return leftTree && rightTree
+ 
+}
