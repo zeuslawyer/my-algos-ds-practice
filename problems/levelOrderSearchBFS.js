0ddd5ce2f961fb46d152c9be5a988f4353536b76
zeuslawyer <zubin.pratap@gmail.com>
2020-02-17 23:40:25 +1100

    tidy

--- problems/levelOrderSearchBFS.js
+++ problems/levelOrderSearchBFS.js
@@ -1,4 +1,5 @@
 // LEETCODE: https://leetcode.com/problems/binary-tree-level-order-traversal/
+// solution /: https://leetcode.com/problems/binary-tree-level-order-traversal/discuss/401728/Javascript-solution-using-BFS
 
 // Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
 
