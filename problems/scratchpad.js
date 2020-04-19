6462132be1b3e729704cb549bf47b982a26acb9a
zeuslawyer <zubin.pratap@gmail.com>
2020-04-19 14:38:32 +1000

    ...

--- problems/scratchpad.js
+++ problems/scratchpad.js
@@ -1,72 +1,37 @@
-function smallestSubstringContaining(big, small) {
-  if (big.length < small.length) return '';
+// https://leetcode.com/problems/sliding-window-maximum/
 
-  let windowStart = 0;
-  let windowEnd = Infinity;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+  const res = [];
+  for (let i = 0; i < nums.length; i++) {
+    let windowStart = i;
+    let windowEnd = k - 1;
 
-  // build reference data
-  const charCountInSmall = {};
-  for (const char of small) {
-    charCountInSmall[char] = charCountInSmall[char]
-      ? ++charCountInSmall[char]
-      : 1;
-  }
-  const numCharsInSmall = Object.keys(charCountInSmall).length;
-
-  // build window dynamic data
-  const charCountInWindow = {};
-  let numCharsInWindow = 0;
-
-  let left = 0;
-  let right = 0;
-
-  while (right < big.length) {
-    let rightChar = big[right];
-    if (!(rightChar in charCountInSmall)) {
-      right++;
-      continue;
-    }
-    // else add to window counts
-    charCountInWindow[rightChar] = charCountInWindow[rightChar]
-      ? ++charCountInWindow[rightChar]
-      : 1;
-
-    if (charCountInWindow[rightChar] === charCountInSmall[rightChar]) {
-      numCharsInWindow++;
-    }
-
-    // have a window with all chars accounted for
-    while (numCharsInWindow === numCharsInSmall && left <= right) {
-      // get window bounds
-      if (right - left < windowEnd - windowStart) {
-        windowStart = left;
-        windowEnd = right;
-      }
-
-      let leftChar = big[left];
-      // check if relevant
-      if (!(leftChar in charCountInSmall)) {
-        left++;
-        continue;
-      }
-
-      // is relevant...
-      if (charCountInWindow[leftChar] === charCountInSmall[leftChar]) {
-        numCharsInWindow--; // decrement overall chars count only if they're all accounted for
-      }
-      charCountInWindow[leftChar]--;
-      left++;
-    }
-
-    right++;
+    const prevMax = res[res.length - 1] || -Infinity;
+    let maxInWindow = getMaxInWindow(windowStart, windowEnd, prevMax, nums);
+    res.push(maxInWindow);
   }
 
-  if (right === Infinity) return ''; // not found
+  return res;
+};
 
-  return big.slice(windowStart, windowEnd + 1);
+function getMaxInWindow(start, end, prevMax, arr) {
+  if (start <= prevMax && end <= prevMax) {
+    console.log(' no change in max, its still', prevMax);
+    return prevMax;
+  }
+
+  let maybeMax = Math.max(arr[start], arr[end]);
+  let newMax = Math.max(prevMax, maybeMax);
+  console.log('prev: ', prevMax, ' and  new:', newMax);
+  return newMax;
 }
 
-const S = 'ADOBECODEBANC';
-const T = 'ABC';
-let a = smallestSubstringContaining(S, T); // BANC
+const nums = [1, 3, -1, -3, 5, 3, 6, 7];
+const k = 3;
+let a = maxSlidingWindow(nums, k); // [3,3,5,5,6,7]
 console.log(a);
