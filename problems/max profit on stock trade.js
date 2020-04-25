3d6f550515d940d7c72049be05769b02b215d0dd
Zubin Pratap <zubin.pratap@gmail.com>
2020-04-25 17:00:37 +1000

    update max profit on stock trade

--- problems/max profit on stock trade.js
+++ problems/max profit on stock trade.js
@@ -12,14 +12,14 @@
  */
 var maxProfit = function (prices) {
   let minPrice = prices[0];
-  let maxProfit = -Infinity;
+  let maxProfit = prices[1] - prices[0];
 
-  for (let i = 0; i < prices.length; i++) {
+  for (let i = 1; i < prices.length; i++) {
     let currentPrice = prices[i];
-    minPrice = Math.min(minPrice, currentPrice);
-
     let profit = prices[i] - minPrice;
+
     maxProfit = Math.max(profit, maxProfit);
+    minPrice = Math.min(minPrice, currentPrice);
   }
 
   return maxProfit;
