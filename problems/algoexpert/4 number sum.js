a1db2262b2e5b20997cfee8ad59507c3f4de2c45
Zubin Pratap <zubin.pratap@gmail.com>
2020-08-23 10:17:53 +1000

    update

--- problems/algoexpert/4 number sum.js
+++ problems/algoexpert/4 number sum.js
@@ -1,58 +1,54 @@
 // Copyright © 2020 AlgoExpert, LLC. All rights reserved.
-2
-​
-3
+
 // Average: O(n^2) time | O(n^2) space
-4
+
 // Worst: O(n^3) time | O(n^2) space
-5
+
 function fourNumberSum(array, targetSum) {
-6
-  const allPairSums = {};
-7
-  const quadruplets = [];
-8
-  for (let i = 1; i < array.length - 1; i++) {
-9
-    for (let j = i + 1; j < array.length; j++) {
-10
-      const currentSum = array[i] + array[j];
-11
-      const difference = targetSum - currentSum;
-12
-      if (difference in allPairSums) {
-13
-        for (const pair of allPairSums[difference]) {
-14
-          quadruplets.push(pair.concat([array[i], array[j]]));
-15
+    const allPairSums = {};
+    const quadruplets = [];
+
+    for (let i = 1; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            const currentSum = array[i] + array[j];
+
+            const difference = targetSum - currentSum;
+
+            if (difference in allPairSums) {
+                for (const pair of allPairSums[difference]) {
+                    quadruplets.push(pair.concat([array[i], array[j]]));
+                }
+            }
         }
-16
-      }
-17
+
+        for (let k = 0; k < i; k++) {
+            const currentSum = array[i] + array[k];
+
+            if (!(currentSum in allPairSums)) {
+                allPairSums[currentSum] = [[array[k], array[i]]];
+            } else {
+                allPairSums[currentSum].push([array[k], array[i]]);
+            }
+        }
     }
-18
-    for (let k = 0; k < i; k++) {
-19
-      const currentSum = array[i] + array[k];
-20
-      if (!(currentSum in allPairSums)) {
-21
-        allPairSums[currentSum] = [[array[k], array[i]]];
-22
-      } else {
-23
-        allPairSums[currentSum].push([array[k], array[i]]);
-24
-      }
-25
-    }
-26
-  }
-27
-  return quadruplets;
+
+    return quadruplets;
 }
-29
-​
-30
-exports.fourNumberSum = fourNumberSum;
+
+let arr = [7, 6, 4, -1, 1, 2];
+
+let targetSum = 16;
+
+let ans = fourNumberSum(arr, targetSum); // [[7, 6, 4, -1], [7, 6, 1, 2]]
+console.log(ans);
+
+let arr2 = [-2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let targetSum2 = 4;
+let ans2 = fourNumberSum(arr2, targetSum2);
+console.log(ans2); //[[-2, -1, 1, 6], [-2, 1, 2, 3], [-2, -1, 2, 5], [-2, -1, 3, 4]]
+
+let arr3 = [1, 2, 3, 4, 5];
+let targetSum3 = 100;
+
+let ans3 = fourNumberSum(arr3, targetSum3);
+console.log(ans3); // []
