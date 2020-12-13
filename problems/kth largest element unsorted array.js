7b9fe1b545a891c0f263cafb364441e5876ce3cd
Zubin Pratap <zubin.pratap@gmail.com>
2020-12-13 12:02:51 +1100

    updated

--- problems/kth largest element unsorted array.js
+++ problems/kth largest element unsorted array.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
 const input = [3, 2, 3, 1, 2, 4, 5, 5, 6];
 const k = 4;
 
@@ -8,6 +10,7 @@ const k = 4;
  */
 var findKthLargest = function (arr, k) {
   buildMaxHeap(arr);
+  console.log(arr)
   for (let i = arr.length - 1; i >= arr.length - k; i--) {
     swap(0, i, arr);
     sinkDown(0, i - 1, arr);
@@ -17,19 +20,19 @@ var findKthLargest = function (arr, k) {
 };
 
 function buildMaxHeap(arr) {
-  let lastParentIdx = Math.floor((arr.length - 2) / 2);
-  for (let parent = lastParentIdx; parent >= 0; parent--) {
+  let lastChildsParentIdx = Math.floor((arr.length - 2) / 2);
+  for (let parent = lastChildsParentIdx; parent >= 0; parent--) {
     sinkDown(parent, arr.length - 1, arr);
   }
 }
 
-function sinkDown(startIdx, lastIdx, arr) {
+function sinkDown(startIdx, stopIdx, arr) {
   let leftChild = 2 * startIdx + 1;
 
-  while (leftChild <= lastIdx) {
+  while (leftChild <= stopIdx) {
     // let rightChild = leftChild + 1;
     let rightChild = 2 * startIdx + 2;
-    if (rightChild > lastIdx) rightChild = -1;
+    if (rightChild > stopIdx) rightChild = -1;
 
     let swapIdx;
     if (rightChild !== -1 && arr[rightChild] > arr[leftChild]) {
@@ -43,7 +46,8 @@ function sinkDown(startIdx, lastIdx, arr) {
       startIdx = swapIdx;
       leftChild = 2 * startIdx + 1;
     } else {
-      return;
+      // sinkdown is over
+      return
     }
   }
 }
@@ -55,8 +59,9 @@ function swap(first, second, arr) {
 }
 
 let a = findKthLargest(input, k); // 4
-let b = findKthLargest([3, 2, 1, 5, 6, 4], 2); // 5
-let c = findKthLargest([7, 6, 5, 4, 3, 2, 1], 5); // 3
-let d = findKthLargest([3, 1, 2, 4], 2);
+// let b = findKthLargest([3, 2, 1, 5, 6, 4], 2); // 5
+// let c = findKthLargest([7, 6, 5, 4, 3, 2, 1], 5); // 3
+// let d = findKthLargest([3, 1, 2, 4], 2);
 
-console.log(a, b, c, d); // 3
+// console.log(a, b, c, d); // 3
+console.log(a); 
--- problems/scratchpad.js
+++ problems/scratchpad.js
@@ -1,31 +1,76 @@
-class BST {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
+function findKthLargest(numsList, k) {
+  buildMaxHeap(numsList)
+  console.log(numsList)
+
+  for (let i=numsList.length-1; i >= numsList.length-k; i--) {
+    swap(0, i, numsList)
+    sinkDown(0, i-1, numsList)
   }
 
-  insert(val) {
-    if (val < this.val) {
-      if (this.left) {
-        this.left.insert(val);
-      } else {
-        this.left = new BST(val);
-      }
+  return numsList[numsList.length - k]
+};
+
+// function buildMaxHeap(numsList){
+//   // find the last parent in the numsListay
+//   let lastElemParentIdx = Math.floor((numsList.length-1-1) / 2)   // using formula childIdx = 2*parentIdx +1 
+
+//   // sink down on all parent elements, to build max heap
+//   for (let i=lastElemParentIdx; i>=0; i--){
+//     sinkDown(i, numsList.length-1, numsList)
+//   }
+// }
+
+
+function buildMaxHeap(numsList) {
+  let lastChildsParentIdx = Math.floor((numsList.length - 2) / 2);
+  for (let parent = lastChildsParentIdx; parent >= 0; parent--) {
+    sinkDown(parent, numsList.length - 1, numsList);
+  }
+}
+
+
+function sinkDown(startIdx, stopIdx, numsList){
+  let leftChildIdx = 2 * startIdx + 1
+
+  while (leftChildIdx <= stopIdx) {
+// TODO rename startIdx to parent?
+
+    let rightChildIdx = 2 * startIdx + 2
+    if (rightChildIdx > stopIdx) rightChildIdx = -1
+
+    // find index to swap with. Max heap, so greater of the two children will give the swap idx
+    let swapIdx = leftChildIdx;
+    if(rightChildIdx !== -1 && numsList[rightChildIdx] > numsList[startIdx]){
+      swapIdx = rightChildIdx
+    }
+
+    // swap if child value is greater than parent
+    if(numsList[startIdx] < numsList[swapIdx]){
+      swap(startIdx , swapIdx, numsList)
+      startIdx = swapIdx
+      leftChildIdx = 2 * startIdx + 1
     } else {
-      // right is > or ==
-      if (this.right) {
-        this.right.insert(val);
-      } else {
-        this.right = new BST(val);
-      }
+      // done with sink down
+      return
     }
   }
 }
 
-const arr = [10, 15, 8, 12, 94, 81, 5, 2, 11];
-const t = new BST(arr[0]);
-for (let i = 1; i < arr.length; i++) {
-  t.insert(arr[i]);
+function swap(idx1, idx2, numsList){
+  let temp = numsList[idx1]
+  numsList[idx1] = numsList[idx2]
+  numsList[idx2] = temp
 }
-console.log(t);
+
+
+
+
+const input = [3, 2, 3, 1, 2, 4, 5, 5, 6];
+const k = 4;
+let a = findKthLargest(input, k); // 4
+// let b = findKthLargest([3, 2, 1, 5, 6, 4], 2); // 5
+// let c = findKthLargest([7, 6, 5, 4, 3, 2, 1], 5); // 3
+// let d = findKthLargest([3, 1, 2, 4], 2);
+
+// console.log(a, b, c, d); 
+console.log(a); 
