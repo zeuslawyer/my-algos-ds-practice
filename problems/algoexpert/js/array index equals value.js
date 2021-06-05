48fd91506de50ef615e56a6a6e8ca24d69877f12
Zubin Pratap <zubin.pratap@gmail.com>
2021-06-05 17:48:50 +1000

    all permutations in arr

--- problems/algoexpert/js/array index equals value.js
+++ problems/algoexpert/js/array index equals value.js
@@ -1,6 +1,10 @@
 // https://www.algoexpert.io/questions/Index%20Equals%20Value
 // https://drive.google.com/file/d/1I1Zo56AEnRv2Dm4FFevl7rbCNwPVKYUb/view?usp=sharing
 
+// Write a function that takes in a sorted array of distinct integers and returns
+// the first index in the array that is equal to the value at that index. In
+// other words, your function should return the minimum index where index == array[index]
+
 function indexEqualsValue(arr) {
     // Write your code here.
     let start = 0;
@@ -13,7 +17,6 @@ function indexEqualsValue(arr) {
 
         if (currVal === midIdx) {
             // check on the left to see if there is previous num that is also == its index
-
             let prevIdx = midIdx - 1;
             let prevVal = arr[prevIdx];
             if (prevVal === prevIdx) {
@@ -48,7 +51,7 @@ const got = indexEqualsValue(tc1);
 function assert(want, got) {
     want === got
         ? console.log("PASS")
-        : console.log(`FAIL:  wanted ${want} but got ${got}}`);
+        : console.log(`FAIL:  wanted ${want} but got ${got}}.`);
 }
 assert(3, got);
 assert(2, indexEqualsValue([-5, 1, 0, 3, 4, 5, 9]));
